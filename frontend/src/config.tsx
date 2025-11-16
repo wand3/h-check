003158dfbfb6dff8307ca9004e63578e07b3de66
wand3 <godswillbaiyewu@gmail.com>
2025-11-16 05:50:26 +0100

    Backend frontend linking

--- frontend/src/config.tsx
+++ frontend/src/config.tsx
@@ -2,7 +2,7 @@ const Config = {
     baseURL: import.meta.env.VITE_API_URL || 
             (import.meta.env.MODE === "development" 
                 ? "http://127.0.0.1:8000" 
-                : "https://localhost:8000")
+                : "https://h-check.onrender.com")
 }
 
 export default Config;
