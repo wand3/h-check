e4f42dfe7f126590aaf208b3eed069fba6dc64e5
wand3 <godswillbaiyewu@gmail.com>
2025-11-17 04:38:58 +0100

    CORS error handling fix

--- app/main.py
+++ app/main.py
@@ -16,9 +16,8 @@ def create_app() -> FastAPI:
 
     origins = [
         "http://localhost:5173/",
-        "http://127.0.0.1:8000/*",
-        "https://h-check.pages.dev/*",
-        "https://h-check.onrender.com/*",
+        "https://h-check.pages.dev/",
+        "https://h-check.onrender.com/",
         "*"
     ]
     app.add_middleware(
